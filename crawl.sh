bd6ffbb07a2e3b81861d17e5873a158b038bd936
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2025-05-26 10:45:36 +0200

    Ajout heure limite de mis à jour du fichier

--- crawl.sh
+++ crawl.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+HEURE_LIMITE=18
+
 while true; do
-  set `date`
-  echo "Run - $*"
-  curl --silent http://localhost:3030/crawl > index.new.html
-  ret=$?
-  if [[ $ret -gt 0 ]]; then
-    echo "Erreur d'exécution"
-    exit 1
-  fi
-  mv ./index.new.html ./public/index.html
+  set `date +%H`
+  if [ "$*" -lt $HEURE_LIMITE ]; then
+    set `date`
+    echo "Run - $*"
+    curl --silent http://localhost:3030/crawl > index.new.html
+    ret=$?
+    if [[ $ret -gt 0 ]]; then
+      echo "Erreur d'exécution ($ret)"
+      exit 1
+    fi
+    mv ./index.new.html ./public/index.html
+  fi 
+  echo "coucou"
 
   sleep 30
 done
